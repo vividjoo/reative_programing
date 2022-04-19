5cead7a405551f53f08cbe765fdf062fc9438048
joo <vividjtj@gmail.com>
2022-04-19 12:54:19 +0900

    api

--- src/main/java/com/webflux/codestates/Hello.java
+++ src/main/java/com/webflux/codestates/Hello.java
@@ -1,27 +1,49 @@
 package com.webflux.codestates;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.function.ServerRequest;
-import org.springframework.web.servlet.function.ServerResponse;
-import org.json.JSONObject;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 
 import java.util.HashMap;
-import java.util.Map;
+
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
 
 @Component
 public class Hello {
 
-    public ServerResponse flux(ServerRequest req) {
-        String name = req.pathVariable("name");
+//    public Mono<ServerResponse> flux(ServerRequest req) {
+//        String name = req.pathVariable("name");
+//
+//
+//        System.out.println("name : " + name);
+//        Map<String, String> map = new HashMap<>();
+//        map.put("message", "hello " + name);
+//        map.put("to", name);
+//        JSONObject json = new JSONObject(map);
+//        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(json.toMap());
+//    }
 
-        System.out.println("name : " + name);
-        Map<String, String> map = new HashMap<>();
-        map.put("message", "hello " + name);
-        map.put("to", name);
-        JSONObject json = new JSONObject(map);
-        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(json.toMap());
+
+    public Mono<ServerResponse> helloHandler (ServerRequest req) {
+        System.out.println("123123123123");
+        String name = req.queryParam("name").get();
+//        String name = req.pathVariable("name");
+        HashMap nameMap = new HashMap();
+        nameMap.put("to", name);
+        nameMap.put("message", "hello "+name);
+        Mono<HashMap> mymono = Mono.just(nameMap);
+        return
+                ServerResponse.ok()
+               .contentType(MediaType.APPLICATION_JSON)
+               .body(BodyInserters.fromProducer(mymono, HashMap.class));
     }
 }
--- src/main/java/com/webflux/codestates/RouterConfig.java
+++ src/main/java/com/webflux/codestates/RouterConfig.java
@@ -2,16 +2,32 @@ package com.webflux.codestates;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.RouterFunctions;
-import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
 
 @Configuration
+@EnableWebFlux
 public class RouterConfig {
 
+//    @Bean
+//    public RouterFunction<ServerResponse> routerExample(Hello hello) {
+//        return RouterFunctions.route().path("/hello", builder -> builder.GET("{name}", request -> hello.helloHandler(request))
+//        ).build();
+//
+////        return (RouterFunction<ServerResponse>) RouterFunctions.route(
+////                RequestPredicates.GET("/hello").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
+////                hello::helloHandler);
+//    }
+
+//   아래 설정
     @Bean
-    public RouterFunction<ServerResponse> routerExample(Hello hello) {
-        return RouterFunctions.route().path("/hello", builder -> builder.GET("{name}", request -> hello.flux(request))
-        ).build();
+    public RouterFunction<ServerResponse> helloRouter(Hello hello) {
+        return RouterFunctions.route()
+                .GET("/hello", hello::helloHandler)
+                .build();
     }
+    //   아래 설정
 }
